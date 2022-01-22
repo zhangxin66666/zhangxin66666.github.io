79f74c30a9802c068f57d1d57b686a5aa0826e89
58 <>
2022-01-22 18:40:01 +0800

    Site updated: 2022-01-22 18:40:01

--- js/chocolate.js
+++ js/chocolate.js
@@ -1,10 +1,3 @@
-/*
- * @Author: tzy1997
- * @Date: 2020-12-15 20:55:25
- * @LastEditors: tzy1997
- * @LastEditTime: 2021-01-12 19:02:25
- */
-
 // 友情链接页面 头像找不到时 替换图片
 if (location.href.indexOf("link") !== -1) {
     var imgObj = document.getElementsByTagName("img");
